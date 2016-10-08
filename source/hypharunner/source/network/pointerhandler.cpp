3682fdab93ef4d55f84c377d778db785c8c7d199
Christian <chriamue@gmail.com>
2016-10-08 15:24:46 +0200

    removed pointercontroller and merged to hypha library

--- source/hypharunner/source/network/pointerhandler.cpp
+++ source/hypharunner/source/network/pointerhandler.cpp
@@ -1,6 +1,7 @@
 // Copyright (c) 2016 Hypha
 
 #include "hypharunner/network/pointerhandler.h"
+#include <hypha/core/cache/cache.h>
 
 #include <Poco/Exception.h>
 #include <Poco/Net/HTMLForm.h>
@@ -12,13 +13,15 @@ using namespace hypha::utils;
 
 void PointerHandler::handleRequest(Poco::Net::HTTPServerRequest &request,
                                    Poco::Net::HTTPServerResponse &response) {
+  std::string uri = request.getURI();
+  std::string uuid = uri.substr(uri.find_last_of("/") + 1);
   try {
     response.setChunkedTransferEncoding(true);
     response.setContentType("text");
 
     std::ostream &ostr = response.send();
 
-    ostr << "";
+    ostr << hypha::cache::Cache::instance()->get(uuid);
   } catch (Poco::Exception &exc) {
     Logger::error(exc.displayText());
   }
